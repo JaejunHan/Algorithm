86a3f2357b051c8b9d5047462f9e8eb9d30b8332
JaejunHan <87077857+JaejunHan@users.noreply.github.com>
2023-11-17 00:50:16 +0900

    [Gold V] Title: 동전 1, Time: 224 ms, Memory: 17796 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static int n, k;
+    static int[] arr;
+    static int[] dp;
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        k = sc.nextInt();
+        arr = new int[n+1];
+        dp = new int[k+1];
+        dp[0] = 1;
+        for (int i=0; i< n; i++) {
+            arr[i] = sc.nextInt();
+            for (int j=arr[i]; j <= k; j++) {
+                dp[j] += dp[j-arr[i]];
+            }
+        }
+
+        System.out.println(dp[k]);
+    }
+}
