84d6a43d23f72474e24cf6829780205784ff6f74
JaejunHan <87077857+JaejunHan@users.noreply.github.com>
2024-01-06 17:52:47 +0900

    [level 5] Title: 시험장 나누기, Time: 11.12 ms, Memory: 56.8 MB -BaekjoonHub

---
+++ 프로그래머스/5/81305. 시험장 나누기/시험장 나누기.java
@@ -0,0 +1,93 @@
+import java.util.*;
+class Solution {
+    int root = -1;
+    int cnt = 0;
+    int[] parent;
+    Node[] nodes;
+    int[] Num;
+    int mid = 0;
+    public int solution(int k, int[] num, int[][] links) {
+        Num = num;
+        parent = new int[num.length];
+        nodes = new Node[num.length];
+        Arrays.fill(parent, -1);
+        setTree(links);
+        int sum = 0;
+        for (int ele: num) {
+            sum += ele;
+        }
+        
+        int low = 0; int high = (int) 1e9;
+        for(int n : num) low = Math.max(low, n);
+        while (low <= high) {
+            mid = (low + high) / 2;
+            if (check() <= k) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+    
+    private int dfs(int n) {
+        Node node = nodes[n];
+        
+        int lft = 0; int rht = 0;
+        
+        if (node.left != -1)
+            lft = dfs(node.left);
+        
+        if (node.right != -1)
+            rht = dfs(node.right);
+        
+        if (Num[n] + lft + rht <= mid) {
+            return Num[n] + lft + rht;
+        }
+        
+        if (Num[n] + Math.min(lft, rht) <= mid) {
+            cnt++;
+            return Num[n] + Math.min(lft, rht);
+        }
+        
+        //둘다 잘라야하는 경우
+        cnt += 2;
+        return Num[n];
+    }
+        
+    private int check() {
+        cnt = 0;
+        dfs(root);
+        return cnt + 1;
+    }
+    
+    private void setTree(int[][] links) {
+        int i=0;
+        for (int[] link: links) {
+            int left = link[0];
+            int right = link[1];
+            nodes[i] = new Node(left, right);
+            if (left != -1) {
+                parent[left] = i;     
+            }
+            if (right != -1) {
+                parent[right] = i;     
+            }
+            i++;
+        }
+        
+        for (int j = 0; j < parent.length; j++) {
+            if (parent[j] == -1) {
+                root = j;
+                break;
+            }
+        }
+    }
+ 
+    public class Node {
+        int left; int right;
+        public Node(int left, int right) {
+            this.left = left; this.right = right;
+        }
+    }
+}
